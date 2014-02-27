d125c0d80cf5c847b7a0d36fa9e0ebe7bfc1a2e1
Ezequiel Pereira <eze2307@gmail.com>
2014-02-27 15:27:32 -0200

    Renombrado pr.cpp a main.cpp

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-SOURCES="pr.cpp load.cpp common.cpp init.cpp draw.cpp cubes.cpp movimiento.cpp"
+SOURCES="main.cpp load.cpp common.cpp init.cpp draw.cpp cubes.cpp movimiento.cpp"
 g++ -Wall -s $SOURCES -o pr `sdl2-config --libs --cflags` -lSDL2_image -lSDL2_ttf -lGL -lGLU -Wl,-rpath -Wl,/usr/local/lib
